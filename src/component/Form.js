480b7439e7a35a79521dca4da0e65751435aee82
Mickael <noxbike@gmail.com>
2022-01-03 09:31:21 +0400

    check and send data to server
    check if input is empty or not and send to server with axios

--- src/component/Form.js
+++ src/component/Form.js
@@ -1,41 +1,94 @@
 import React, { useState} from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import { TextField } from '@material-ui/core/';
-import { Button } from '@material-ui/core';
-import { Checkbox } from '@material-ui/core';
-import { FormControlLabel } from '@material-ui/core';
+import { TextField, Checkbox, Button, FormControlLabel, makeStyles } from '@material-ui/core/';
+import { Alert } from '@material-ui/lab';
+import axios from 'axios';
+const serveur = "http://localhost:3000";
 
 const useStyles = makeStyles((theme) => ({
     root: {
         '& > *': {
             margin: theme.spacing(2),
-            width:'46ch',
+            width:'38ch',
         },
     },
 }));
 
-
-
 export default function Form() {
-    const [checked, setChecked] = useState(false)
-    const classes = useStyles()
+    const [remember, setRemember] = useState(false);
+    const [email, setEmail] = useState(null);
+    const [password, setPassword] = useState(null);
+    const [error, setError] = useState({email: false, password: false, check: null});
+    const classes = useStyles();
+
+    const LoginUser = () => {
+        if(!email || !password){
+            let error = {email: false, password: false};
+            error.email = !email ? true : false;
+            error.password = !password ? true : false;
+            return setError(error);
+        }
+
+        axios.post(`${serveur}/login`, {
+            'email': email,
+            'password': password,
+            'remember': remember,
+        })
+        .then(function(response){
+            if(response.data.success){
+                alert(response.data.success)
+            }
+            else if(response.data.error){
+                setError({email: false, password: false, check: response.data.error})
+            }
+            else{
+                setError({email: false, password: false, check: response.data.err})
+            }
+            
+        })
+        .catch(function(error){
+            console.log(error);
+        })
+    }
 
     return (
-        <form className={classes.root + " login_form"} noValidate autoComplete='off'>
+        <form className={classes.root + " login_form"} noValidate autoComplete='off' onSubmit={() => LoginUser()}>
 
-            <TextField id="outlined-basic" label="Email" variant="outlined" type='text' name='Email' placeholder='Enter your email'/>
+            {error.check && <Alert variant="outlined" severity="warning">
+                {error.check}
+            </Alert>}
+            <TextField 
+                error= {error.email ? true : false}
+                helperText={error.email ? "Please enter your email." : null}
+                id="outlined-basic"
+                label="Email"
+                variant="outlined"
+                type='text' 
+                name='Email'
+                placeholder='Enter your email'
+                onChange={(e) => setEmail(e.target.value)}
+            />
 
-            <TextField id="outlined-basic" label="password" variant="outlined" type='password' name='password' placeholder='Enter your password'/>
-            
+            <TextField 
+                error={error.password ? true : false}
+                helperText={error.password ? "Please enter your password." : null}
+                id="outlined-basic"
+                label="password"
+                variant="outlined"
+                type='password'
+                name='password'
+                placeholder='Enter your password'
+                onChange={(e) => setPassword(e.target.value)}
+            />
+
             <FormControlLabel
                 value="end"
                 control={<Checkbox color="primary" />}
                 label="Remember me"
                 labelPlacement="end"
-                onClick={() => setChecked(!checked)}
+                onClick={() => setRemember(!remember)}
             />
 
-            <Button variant="contained" color="primary" disableElevation>
+            <Button type="submit" variant="contained" color="primary" disableElevation>
                 Login
             </Button>
 
